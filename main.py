eca01daf1611167b3dfa1ed848e84d1615c01b29
rufatbayramov90 <78435344+rufatbayramov90@users.noreply.github.com>
2023-10-09 20:34:04 +0400

    Add  ASCII

--- main.py
+++ main.py
@@ -1,11 +1,71 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+
+lives = 6
 display = []
 word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 print(display)
+
 end_of_game = False
 while not end_of_game:
     guess = input("Guess a letter").lower()
@@ -13,8 +73,17 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True  
+            print("You lose")
+
     print(display)
     if "_" not in display:
         end_of_game = True
         print("You win")
+
+    print(stages[lives])
     
